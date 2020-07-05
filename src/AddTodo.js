39ed722463ad45716aa2db903f6d300c205e0d93
tuhinchakraborty <tuhin_chakraborty@outlook.com>
2020-07-05 14:10:59 +0530

    add styling

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import "./App.css";
 
 class AddTodo extends Component {
   constructor() {
@@ -10,18 +11,19 @@ class AddTodo extends Component {
   }
 
   handleKeyPress = (target) => {
-    if(target.key === 'Enter') {
-        const todoText = this.textInput.current.value
-        this.props.todoText(todoText)
+    if (target.key === "Enter") {
+      const todoText = this.textInput.current.value;
+      this.props.todoText(todoText);
     }
-  }
+  };
 
   render() {
     return (
       <>
-        <Jumbotron>
+        <Jumbotron className="Todo-text-section">
           <InputGroup>
             <FormControl
+              className="Todo-form-control"
               aria-describedby="inputGroup-sizing-sm"
               ref={this.textInput}
               type="text"
